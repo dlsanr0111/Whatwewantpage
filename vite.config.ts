86c82a93701316cf0d48e6d8fdcfaa7298870c67
mook <cim0111@naver.com>
2025-11-14 16:01:24 +0900

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/Whatwewnatpange/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
